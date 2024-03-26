d528db3c867955a69e0c6c5d9fe8b8ce20b2c678
nicknamed <nicknamed19@gmail.com>
2024-03-26 12:17:03 -0500

    Se crea la funcion para mostrar dinamicamente las peliculas en tendencia utilizando la informacion de la API

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-console.log('hello, world ' + API_KEY);
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    
+    const movies = data.results;
+    const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+    movies.forEach(film => {
+        const filmContainer = document.createElement('div');
+        filmContainer.classList.add('movie-container');
+
+        const filmImg = document.createElement('img');
+        filmImg.classList.add('movie-img');
+        filmImg.alt = film.title;
+        filmImg.src = 'https://image.tmdb.org/t/p/w300' + film.poster_path;
+
+        filmContainer.appendChild(filmImg);
+        trendingPreviewContainer.appendChild(filmContainer);
+
+
+    });
+};
+
+getTrendingMoviesPreview()
