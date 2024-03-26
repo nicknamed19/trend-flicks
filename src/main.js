05acc724d7047d2587613895849b56087a3d7b44
nicknamed <nicknamed19@gmail.com>
2024-03-26 15:12:42 -0500

    Se hace la migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+'use strict'
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        'Content-type': 'application/json;charset=utf-8',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTQ0NTc4MTk5YjczYjdlZmIzYmJiNmU4NDI0YzhiNyIsInN1YiI6IjY2MDFlOWM0NjJmMzM1MDE2NDUyMzJmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y-Z6z9V_Qq71CgbV3yIbxZdLtOp0MoeL_rjhq7ZTb4g',
+    }
+})
+
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
+    const {data, status} = await api('/trending/movie/day');
+    
     
     const movies = data.results;
     const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
@@ -21,4 +31,26 @@ async function getTrendingMoviesPreview() {
     });
 };
 
+async function getCategoriesPreview() {
+    const {data, status} = await api('/genre/movie/list');
+
+    const categoriesPreview = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    const categories = data.genres;
+
+    categories.forEach(category => {
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container')
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.id = `id${category.id}`;
+        categoryTitle.classList.add('category-title');
+        categoryTitle.innerText = category.name;
+
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreview.appendChild(categoryContainer);
+    })
+    
+}
+
+getCategoriesPreview()
 getTrendingMoviesPreview()
